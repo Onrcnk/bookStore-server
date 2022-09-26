92442aa3b1737d6050aa4b1f5801f2e230c65a03
Onrcnk <onrcnk@hotmail.com>
2022-09-26 20:33:12 +0300

    first commit

---
+++ src/main/java/com/onrcnk/bukunuz/BukunuzApplication.java
@@ -0,0 +1,13 @@
+package com.onrcnk.bukunuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukunuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukunuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onrcnk/bukunuz/BukunuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onrcnk.bukunuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukunuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
